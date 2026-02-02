94e440f0a9e5b6735d6c155eb8d004d5ccb58f3f
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-02 22:22:36 +0100

    Db context

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Data;
+
+public class ApplicationDbContext : DbContext
+{
+   public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+   {
+
+   }
+   public DbSet<Board> Boards {get; set;}
+   public DbSet<Topic> Topics {get; set;}
+   public DbSet<Post> Posts {get; set;}
+}
