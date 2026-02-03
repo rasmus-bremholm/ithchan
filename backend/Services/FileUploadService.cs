783b6c5e7f9e7d7cc62f7555fe6f93a4883afbd0
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-03 19:31:45 +0100

    Fileupload service
    Created not tested.

--- backend/Services/FileUploadService.cs
+++ backend/Services/FileUploadService.cs
@@ -12,6 +12,41 @@ public class FileUploadService
    //Constructor
    public FileUploadService(IWebHostEnvironment env)
    {
+      // Sets the uploadspath to be in the /uploads dir
+      _uploadsPath = Path.Combine(env.WebRootPath, "uploads");
+      _thumbnailsPath = Path.Combine(env.WebRootPath, "uploads", "thumbnails");
 
+      Directory.CreateDirectory(_uploadsPath);
+      Directory.CreateDirectory(_thumbnailsPath);
+   }
+
+   public async Task<(string imagePath, string thumnailPath)> SaveImagesAsync(IFormFile file)
+   {
+      // Basically generate a filename and file ending from the uploaded file.
+      var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+      var fullPath = Path.Combine(_uploadsPath, fileName);
+      var thumbnailFileName = $"thumb_{fileName}";
+      var thumbnailFullPath = Path.Combine(_thumbnailsPath, thumbnailFileName);
+
+
+      // Save Image to Disk
+      using (var stream = new FileStream(fullPath, FileMode.Create))
+      {
+         await file.CopyToAsync(stream);
+      }
+
+      //Generate thumbnail
+      using (var image = await Image.LoadAsync(fullPath))
+      {
+         image.Mutate(x => x.Resize(new ResizeOptions
+         {
+            Size = new Size(250, 250),
+            Mode = ResizeMode.Max
+         }));
+
+         await image.SaveAsync(thumbnailFullPath);
+      }
+
+      return ($"uploads/{fileName}", $"uploads/thumbnails/{thumbnailFileName}");
    }
 }
