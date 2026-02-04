fbd1f1bacca83ce0ee975e18421fd71cf1d5c949
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-04 13:56:36 +0100

    Register method

--- backend/Services/AuthService.cs
+++ backend/Services/AuthService.cs
@@ -21,4 +21,27 @@ public class AuthService
       _context=context;
       _configuration=configuration;
    }
+
+   // Register Method MAY take in a User, but no need to.
+   public async Task<User?> RegisterAsync(string username, string password, UserRole role)
+   {
+      if(await _context.Users.AnyAsync(u => u.Username == username))
+      {
+         return null;
+      }
+
+      string passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+      var user = new User
+      {
+        Username = username,
+        PasswordHash = passwordHash,
+        Role = role,
+        CreatedAt = DateTime.UtcNow
+      };
+
+      _context.Users.Add(user);
+      await _context.SaveChangesAsync();
+
+      return user;
+   }
 }
