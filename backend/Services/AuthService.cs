8a1b164ec807418709bf6cd214b87cc45632e426
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-04 13:41:25 +0100

    Create AuthService.cs

---
+++ backend/Services/AuthService.cs
@@ -0,0 +1,14 @@
+using backend.Data;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.Services;
+
+public class AuthService
+{
+
+}
