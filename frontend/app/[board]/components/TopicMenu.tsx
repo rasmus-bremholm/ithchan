16057325910e1e0c5c4f85e93c4e4642221d6424
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-19 23:09:19 +0100

    Added Admin Controls

--- frontend/app/[board]/components/TopicMenu.tsx
+++ frontend/app/[board]/components/TopicMenu.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { IconButton, Menu, MenuItem } from "@mui/material";
+import { IconButton, Menu, MenuItem, Divider, Box } from "@mui/material";
 import { MoreVert } from "@mui/icons-material";
 import { useState, MouseEvent } from "react";
 
 export default function TopicMenu() {
 	const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
 	const open = Boolean(anchorElement);
+	const isAdmin = true;
 
 	const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
 		setAnchorElement(event.currentTarget);
@@ -23,8 +24,20 @@ export default function TopicMenu() {
 			</IconButton>
 
 			<Menu anchorEl={anchorElement} open={open} onClose={handleClose}>
-				<MenuItem>Hide Thread</MenuItem>
-				<MenuItem>Report Thread</MenuItem>
+				<MenuItem onClick={handleClose}>Hide Thread</MenuItem>
+				<MenuItem onClick={handleClose}>Report Thread</MenuItem>
+				{isAdmin && [
+					<Divider key='divider' flexItem orientation='horizontal' sx={{ my: 1, borderColor: "text.secondary", opacity: 0.3 }} />,
+					<MenuItem key='pin' onClick={handleClose}>
+						Pin Thread
+					</MenuItem>,
+					<MenuItem key='lock' onClick={handleClose}>
+						Lock Thread
+					</MenuItem>,
+					<MenuItem key='delete' onClick={handleClose}>
+						Delete Thread
+					</MenuItem>,
+				]}
 			</Menu>
 		</>
 	);
