cd1ac33d91bad66c3bca5061dc09abee5202c1f4
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-19 23:44:50 +0100

    ImageData
    Trying to make Image Metadata into its own model.

---
+++ backend/Models/ImageData.cs
@@ -0,0 +1,11 @@
+namespace backend.Models;
+
+public class ImageData
+{
+   public string ImagePath {get; set;}
+   public string ThumbNailPath {get; set;}
+   public int ImageWidth {get; set;}
+   public int ImageHeight {get; set;}
+   public long ImageSize {get; set;}
+   public string ImageFormat {get; set;}
+}
