7f813acea313a0d21d438b85e462106904991b91
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-19 22:09:48 +0100

    Thread Sorting

--- frontend/app/[board]/components/TopicList.tsx
+++ frontend/app/[board]/components/TopicList.tsx
@@ -1,7 +1,6 @@
 import getTopicsOnBoard from "@/app/actions/getTopicsOnBoard";
 import TopicCard from "./TopicCard";
 
-
 interface TopicListProps {
 	board: string;
 }
@@ -10,9 +9,20 @@ export default async function TopicList({ board }: TopicListProps) {
 	// This component is responsiable for fetching and rendering topics on the server.
 	const topics = await getTopicsOnBoard(board);
 
+	{
+		/* Here we need to handle topic sorting, and hidden topics */
+	}
+	const sortedTopics = topics.sort((a, b) => {
+		if (a.isPinned && !b.isPinned) return -1;
+		if (!a.isPinned && b.isPinned) return 1;
+		return new Date(b.lastBumpedAt).getTime() - new Date(a.lastBumpedAt).getTime();
+	});
+
+	console.log(sortedTopics);
+
 	return (
 		<>
-			{topics.map((topic) => (
+			{sortedTopics.map((topic) => (
 				<TopicCard key={topic.id} topic={topic} />
 			))}
 		</>
