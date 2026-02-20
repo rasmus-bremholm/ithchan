c36db844531b4b23c261871fb7e11438d805df87
Rasmus Bremholm <rasmus.brem@gmail.com>
2026-02-20 09:20:19 +0100

    Image Data & Updates Post

--- backend/Models/ImageData.cs
+++ backend/Models/ImageData.cs
@@ -2,10 +2,13 @@ namespace backend.Models;
 
 public class ImageData
 {
-   public string ImagePath {get; set;}
-   public string ThumbNailPath {get; set;}
+   public int Id {get; set;}
+   public string ImagePath {get; set;} = string.Empty;
+   public string ThumbNailPath {get; set;} = string.Empty;
    public int ImageWidth {get; set;}
    public int ImageHeight {get; set;}
    public long ImageSize {get; set;}
-   public string ImageFormat {get; set;}
+   public string ImageFormat {get; set;} = string.Empty;
+   public int PostId {get; set;}
+   public Post Post {get; set;} = null!;
 }
--- backend/Models/Post.cs
+++ backend/Models/Post.cs
@@ -7,8 +7,8 @@ public class Post
    public int? UserId {get; set;}
    public string Name {get; set;} = "Anonymous";
    public string Content {get; set;} = string.Empty;
-   public string? ImagePath {get;set;}
-   public string? ThumbnailPath {get; set;}
+   public int? ImageDataId {get;set;}
+   public ImageData? ImageData {get; set;}
    public bool IsDeleted {get; set;} = false;
    public DateTime CreatedAt {get; set;}
    public Topic Topic {get; set;} = null!;
